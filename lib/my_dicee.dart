7673eb5c895280137664aedf0ed15868e3fbfccc
Jordan-erifried <danerifried@gmail.com>
2022-01-26 19:38:32 +0000

    Some code in dicee

--- lib/my_dicee.dart
+++ lib/my_dicee.dart
@@ -1,10 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyDicee());
-}
-
 class MyDicee extends StatefulWidget {
   const MyDicee({Key? key}) : super(key: key);
 
@@ -15,8 +11,8 @@ class MyDicee extends StatefulWidget {
 class _MyDiceeState extends State<MyDicee> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
-  final ButtonStyle style = ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
-  void left()  {
+
+  void left() {
     setState(() {
       leftDiceNumber = Random().nextInt(6) + 1;
       rightDiceNumber = Random().nextInt(6) + 1;
@@ -31,48 +27,34 @@ class _MyDiceeState extends State<MyDicee> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dicee'),
-        backgroundColor: Colors.teal,
-        centerTitle: true,
-      ),
-      backgroundColor: Colors.teal,
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                children: <Widget>[
-                  Expanded(
-                   child: Padding(
-                     padding: const EdgeInsets.all(6.0),
-                     child: Image.asset('images/dice$leftDiceNumber.png'),
-                   ),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.red,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: FlatButton(
+                      onPressed: () {
+                        left();
+                      },
+                      child: Image.asset('images/dice$leftDiceNumber.png')
                   ),
+                ),
 
-                  Expanded(
-                   child: Padding(
-                     padding: const EdgeInsets.all(6.0),
-                     child: Image.asset('images/dice$rightDiceNumber.png'),
-                   ),
+                Expanded(
+                  child: FlatButton(
+                      onPressed: () {
+                        right();
+                      },
+                      child: Image.asset('images/dice$rightDiceNumber.png')
                   ),
-                ],
-              ),
-
-              const SizedBox(
-                height: 20.0,
-              ),
-              ElevatedButton(
-                child: const Text('RUN PROGRAM'),
-                onPressed: () {
-                  left();
-                  right();
-                },
-              )
-            ],
-          ),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
