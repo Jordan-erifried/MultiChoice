63b49e823bb9c73ba2decb1dbf3af165fbdd7838
Jordan-erifried <danerifried@gmail.com>
2022-02-02 07:47:57 +0000

    Adding Drawer header

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:multi_choice/ask_me.dart';
-import 'package:multi_choice/my_card.dart';
 
-import 'my_dicee.dart';
-
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: MainPage(),
-      ),
+        ),
     );
   }
 }
 
 class MainPage extends StatelessWidget {
-  const MainPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-              onPressed:() {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const MyCard(),
-                  )
-                );
-              },
-              child: const Text(
-                  'MY CARD',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 20.0,
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          DrawerHeader(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Multi Choice App',
+                  style: TextStyle(
+                      fontSize: 35.0
+                  ),
                 ),
-              ),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.teal,
-              ),
-            ),
 
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const MyDicee(),
-                  )
-                );
-              },
-              child:  const Text(
-                  'MY DICEE',
-                style: TextStyle(
-                    fontFamily: 'Pacifico',
-                  fontSize: 20.0,
-                ),
-              ),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red,
-              ),
+              ],
             ),
-
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const AskMe(),
-                  )
-                );
-              },
-              child: const Text(
-                  'ASK MEE',
-                style: TextStyle(
-                    fontFamily: 'Pacifico',
-                  fontSize: 20.0,
-                ),
-              ),
-            )
-          ],
-        ),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              //image: AssetImage(),
+            ),),
+        ],
       ),
     );
   }
 }
-
 
