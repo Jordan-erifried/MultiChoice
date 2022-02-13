364208fbe8f3c356e31cd51d9d9e967be2e94ff5
Jordan-erifried <danerifried@gmail.com>
2022-02-13 17:47:54 +0000

    replace all the code

--- lib/my_card.dart
+++ lib/my_card.dart
@@ -1,108 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:multi_choice/main.dart';
+
+void main() {
+  runApp(MyCard());
+}
 
 class MyCard extends StatelessWidget {
   const MyCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.teal,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const CircleAvatar(
-              backgroundImage: AssetImage('images/erd.JPG'),
-              radius: 50.0,
-            ),
-            const Text(
-                'Jodan Erifried',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontSize: 20.0,
-                fontFamily: 'Pacifico',
-              ),
-            ),
-
-            Text(
-                'FLUTTER DEVELOPPER',
-              style: TextStyle(
-                color: Colors.teal.shade100,
-                fontSize: 15.0,
-                fontFamily: 'Source Sans Pro',
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            SizedBox(
-              height: 15.0,
-              width: 150.0,
-              child: Divider(
-                color: Colors.teal.shade100,
-              ),
-            ),
-
-            Card(
-              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: ListTile(
-                leading: const Icon(
-                  Icons.phone,
-                  color: Colors.teal,
-                ),
-                title: Text(
-                  '+225 07 893 123 57',
-                  style: TextStyle(
-                    color: Colors.teal.shade900,
-                    fontSize: 20.0,
-                    fontFamily: 'Source Sans Pro',
-                  ),
-                ),
-              ),
-            ),
-
-            Card(
-              margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: ListTile(
-                leading: const Icon(
-                  Icons.mail,
-                  color: Colors.teal,
-                ),
-                title: Text(
-                  'danerifried@gmail.com',
-                  style: TextStyle(
-                    color: Colors.teal.shade900,
-                    fontSize: 20.0,
-                    fontFamily: 'Source Sans Pro',
-                  ),
-                ),
-              ),
-            ),
-
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>  MyApp(),
-                  )
-                );
-              },
-              child: const Text(
-                'GoToHome',
-                style: TextStyle(
-                  fontFamily:'Source Sans Pro',
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            )
-
-          ],
-        ),
-      ),
-    );
+    return Container();
   }
 }
