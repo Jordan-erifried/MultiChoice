8077b450f4d74df8cb556403134c9a046db48a56
Jordan-erifried <danerifried@gmail.com>
2022-02-13 23:46:30 +0000

    Some changement

--- lib/my_dicee.dart
+++ lib/my_dicee.dart
@@ -1,39 +1,12 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyDicee());
-}
+class MyDicee extends StatelessWidget {
+  const MyDicee({Key? key}) : super(key: key);
 
-class MyDicee extends StatefulWidget {
-
-
-  @override
-  State<MyDicee> createState() => _MyDiceeState();
-}
-
-class _MyDiceeState extends State<MyDicee> {
   @override
   Widget build(BuildContext context) {
-    int leftDiceNumber = 1;
-    int rightDiceNumber = 1;
-
-    void leftNumb() {
-      setState(() {
-        leftDiceNumber = Random().nextInt(6) + 1;
-        rightDiceNumber = Random().nextInt(6) + 1;
-      });
-    }
-
-    void rightNumb() {
-      setState(() {
-        rightDiceNumber = Random().nextInt(6) + 1;
-        leftDiceNumber = Random().nextInt(6) + 1;
-      });
-    }
-    return SafeArea(
-      child:
-    );
+   return SafeArea(
+     child: Scaffold(),
+   );
   }
 }
